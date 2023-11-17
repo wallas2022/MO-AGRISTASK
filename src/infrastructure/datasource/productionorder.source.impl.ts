cf16eca3359e763b05bc9d43bec9a5bc25fcccf2
wallas2022 <116705801+wallas2022@users.noreply.github.com>
2023-11-17 11:45:46 -0600

    reconocer tabla

--- src/infrastructure/datasource/productionorder.source.impl.ts
+++ src/infrastructure/datasource/productionorder.source.impl.ts
@@ -10,13 +10,13 @@ import { Productionorder, productionorderDataSource } from "../../domain";
 export class ProductionorderDatasourceImpl implements productionorderDataSource {
 
     async getAll(): Promise<Productionorder[]> {
-         const orderps = await prisma.fcpy_work_centers.findMany();
+         const orderps = await prisma.fcpy_production_orders.findMany();
          return orderps.map( order => Productionorder.fromObject(order))
         }
     
     
      async findById(id: number): Promise<Productionorder> {
-         const orderp = await  prisma.fcpy_work_centers.findFirst({
+         const orderp = await  prisma.fcpy_production_orders.findFirst({
               where: { id }
              });
              if (!orderp) throw `Production order  With id ${ id } not found`;
